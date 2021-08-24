bf717db3ab8cbecf3ebdbdf77cbac5f729ecb3c8
Coletinn <gustavocoleto@hotmail.com>
2021-08-24 18:10:42 -0300

    Controlando os pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas  = 1
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -15,6 +16,15 @@ function posicaoRandomica() {
     // remover o mosquito anterior
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        //console.log('elemento selecionado foi: v' + vidas)
+
+        if (vidas > 3) {
+            alert('Interromper o jogo (game over)')
+        } else {
+            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+            vidas++
+        }
     }
     
 
@@ -43,6 +53,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 
